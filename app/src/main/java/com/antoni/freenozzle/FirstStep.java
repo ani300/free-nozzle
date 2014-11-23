83c0f926a770b1b8083a198e7afd6ed96a3fcd5d
Antoni Virós Martín <ani300@gmail.com>
2014-11-23 23:31:16 +0100

    First things of the app added!

--- app/src/main/java/com/antoni/freenozzle/FirstStep.java
+++ app/src/main/java/com/antoni/freenozzle/FirstStep.java
@@ -1,24 +1,24 @@
 package com.antoni.freenozzle;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class FirstStep extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first_step);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_first_step, menu);
         return true;
     }
 
