bf3eb50f1ea436d49bd29199faaba85e0446dc2e
Antoni Virós Martín <ani300@gmail.com>
2014-12-07 01:04:07 +0100

    Added all the files of the Study Branch and most of the Design one. The UI is pretty basic, there is a lot of work needed in it to make it easy to the eyes. The logic has started to be implemented.

--- app/src/main/java/com/antoni/freenozzle/MainActivity.java
+++ app/src/main/java/com/antoni/freenozzle/MainActivity.java
@@ -3,25 +3,41 @@ package com.antoni.freenozzle;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+    static final String mOperation = "OPERATION";
+    static final int mDesign = 1;
+    static final int mStudy = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mNext = (Button) findViewById(R.id.button);
+        Button mDesign = (Button) findViewById(R.id.design_button);
+        Button mStudy = (Button) findViewById(R.id.study_button);
 
-        mNext.setOnClickListener(new View.OnClickListener() {
+        mDesign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, GasCombustionActivity.class);
+                intent.putExtra(mOperation, MainActivity.mDesign);
+                startActivity(intent);
+            }
+        });
+
+        mStudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GasCombustionActivity.class);
+                intent.putExtra(mOperation, MainActivity.mStudy);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/antoni/freenozzle/StudyActivity.java
+++ app/src/main/java/com/antoni/freenozzle/StudyActivity.java
@@ -1,53 +1,36 @@
 package com.antoni.freenozzle;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 
 
-public class GasCombustionActivity extends ActionBarActivity {
+public class StudyActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gas_combustion);
+        setContentView(R.layout.activity_study);
 
-        Spinner mSpinner = (Spinner) findViewById(R.id.spinner);
+        Spinner mSpinner = (Spinner) findViewById(R.id.spinner2);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-            R.array.rormm_array, android.R.layout.simple_spinner_item);
+                R.array.mord_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         mSpinner.setAdapter(adapter);
-
-        Button mButton = (Button) findViewById(R.id.button);
-
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(GasCombustionActivity.this, StudyActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Log.v("WOLOLO", String.valueOf(this.getIntent().getIntExtra(MainActivity.mOperation, 0)));
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_gas_combustion, menu);
+        getMenuInflater().inflate(R.menu.menu_study, menu);
         return true;
     }
 
